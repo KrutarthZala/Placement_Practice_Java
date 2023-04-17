25d0d5d532075a549d0bb664d146579026c60a51
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-17 23:31:03 +0530

    Change in second for loop of first pattern

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -8,7 +8,7 @@ public class Pattern1 {
         int row = sc.nextInt();
 
         for (int i = 1; i <= row; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
