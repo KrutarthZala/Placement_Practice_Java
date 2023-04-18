a960bb90fb09dff160f1ac429ea2e7d5850cd969
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-19 00:37:38 +0530

    Do Pattern3 and Pattern4 Programs

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter row : ");
+        int row = sc.nextInt();
+
+        int space = row - 1;
+        int star = 1;
+
+        for (int i = 1; i <= row; i++) {
+            
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= star; j++) {
+                System.out.print("* ");
+            }
+            
+            space--;
+            star++;
+            System.out.println();
+        }
+
+    }
+}
---
+++ Patterns/Pattern4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter row : ");
+        int row = sc.nextInt();
+
+        int star = row;
+        int space = 0;
+
+        for (int i = 1; i <= row; i++) {
+
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= star; j++) {
+                System.out.print("* ");
+            }
+
+            star--;
+            space++;
+            System.out.println();
+        }
+    }
+}
