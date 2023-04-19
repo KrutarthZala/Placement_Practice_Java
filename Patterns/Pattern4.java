de52f51b99736d9367b132a3927940dcbd148dfa
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-19 08:54:12 +0530

    Write comment in Patter3 and 4 for space logic

--- Patterns/Pattern4.java
+++ Patterns/Pattern4.java
@@ -13,7 +13,7 @@ public class Pattern4 {
         for (int i = 1; i <= row; i++) {
 
             for (int j = 1; j <= space; j++) {
-                System.out.print("  ");
+                System.out.print("  "); //*Here two spaces equals to one space otherwise it will print Pyramid. */
             }
 
             for (int j = 1; j <= star; j++) {
