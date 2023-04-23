712088736951480fbce0e62628b3b4b361394e8a
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-23 23:59:30 +0530

    Do Count Digit Program

---
+++ Other Programs/CountDigit.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthStyle;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
+
+        int dig = 0 ;
+        while (num != 0) {
+            num /= 10;
+            dig++;
+        }
+
+        System.out.print("Digits are "+dig);
+    }
+}
