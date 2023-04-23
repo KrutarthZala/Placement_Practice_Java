d0368a1859ce57314589f0e08519fe3c9a536cb9
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-24 00:01:08 +0530

    Remove Extra import

--- Other Programs/CountDigit.java
+++ Other Programs/CountDigit.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthStyle;
-
 public class CountDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
