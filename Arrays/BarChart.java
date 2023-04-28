ffc4077d84c0f8233c45e0399362d2ebf300e79f
Krutarth Zala <krutarthzala2@gmail.com>
2023-04-28 21:35:17 +0530

    Bar Chart Using array

---
+++ Arrays/BarChart.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BarChart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element : ");
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        for (int floor = max; floor >= 1; floor--) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= floor) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
